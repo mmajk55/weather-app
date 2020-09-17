daecc80f4a2d4d8a2d115b963d90ab14f9f2a574
michal <michal.malec@softwarehut.com>
2020-09-17 22:12:43 +0200

    Add moment js

--- src/utils/groupData.ts
+++ src/utils/groupData.ts
@@ -8,19 +8,6 @@ export const groupDays = (weatherForecastData) => {
   return groupedData;
 };
 
-export const getDayName = (date) => {
-  const dateObject = new Date(date);
-
-  return dateObject.toLocaleDateString('pl', { weekday: 'long' });
-};
-
-export const getTime = (date) => {
-  const dateObject = new Date(date);
-  const hours = dateObject.getHours();
-
-  return hours ? hours : date;
-};
-
 export const getMax = (arr) => Math.round(Math.max(...arr.map((item) => item.main.temp)));
 
 export const getMin = (arr) => Math.round(Math.min(...arr.map((item) => item.main.temp)));
