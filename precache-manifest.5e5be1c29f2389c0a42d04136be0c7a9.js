4af505d9761f5d025142e9d76ec25335cbe26ba3
michal <michal.malec@softwarehut.com>
2020-09-17 21:35:59 +0200

    Updates

---
+++ precache-manifest.5e5be1c29f2389c0a42d04136be0c7a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c9b91d7b92694e4f58bd71f242b089",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "1dbb899d527d261452f1",
+    "url": "/weather-app/static/js/2.e19556bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.e19556bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4f304277598e089e21",
+    "url": "/weather-app/static/js/main.e4fdbd58.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
